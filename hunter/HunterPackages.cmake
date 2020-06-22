e1ebbce33dcaa4331e7dfb601985b5f536832a86
Yakov Gazizov <imcuber@live.ru>
2020-06-22 19:18:55 +0500

    Cut third-party getters to separate file

---
+++ hunter/HunterPackages.cmake
@@ -0,0 +1,11 @@
+hunter_add_package(glfw)
+find_package(glfw3 REQUIRED)
+
+hunter_add_package(glew)
+find_package(glew REQUIRED)
+
+hunter_add_package(stb)
+find_package(stb CONFIG REQUIRED)
+
+hunter_add_package(Assimp)
+find_package(Assimp REQUIRED)
